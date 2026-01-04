c2d0fc1fad8a9dc1b904e90e2209329e3ff2b6e0
Mark Oborne <markoborne1@gmail.com>
2026-01-04 21:56:34 +0900

    feat: Add zig and zls

--- apps.lua
+++ apps.lua
@@ -11,7 +11,29 @@ App {
     name = "neovim",
     repo = "https://github.com/neovim/neovim.git",
     full_depth = true,
-    make_args = "CMAKE_BUILD_TYPE=RelWithDebInfo",
+    make_args = "CMAKE_BUILD_TYPE=Release",
+}
+
+App {
+    name = "zig",
+    custom = function()
+        local curl <close> = assert(io.popen([[curl -fsSL https://ziglang.org/download/index.json | jq -r '.master["x86_64-linux"].tarball']]))
+        local curl_url = curl:read("*all")
+        local untar_dir = "zig_release"
+        assert(os.execute(string.format("curl %q -o %q", curl_url, untar_dir)))
+        local dir_name = curl_url:match("([^/]+).tar.xz")
+        assert(os.execute(string.format("tar -xJf %q %q/zig --strip-components=1", untar_dir, dir_name)))
+        assert(os.execute(string.format("tar -xJf %q %q/lib --strip-components=1", untar_dir, dir_name)))
+        assert(os.execute("sudo mv -i zig /usr/local/bin/zig"))
+        assert(os.execute("sudo mv -i lib /usr/local/lib/zig"))
+        assert(os.execute(string.format("rm %q", untar_dir)))
+    end
+}
+
+App {
+    name = "zls",
+    repo = "https://github.com/zigtools/zls.git",
+    zig = true,
 }
 
 App {
--- install.lua
+++ install.lua
@@ -2,20 +2,31 @@ function App(app)
     if arg[1] and app.name ~= arg[1] then
         return
     end
-    local dir = "/tmp/luasys_install_" .. app.name
+
+    -- Consider passing in helpful values, e.g. temp file name
+    if app.custom then
+        app.custom()
+        return
+    end
+
+    -- Allow specifying a non temporary path. Useful for things that take a long time to build but support incremental/ cached builds.
+    local dir = app.path or ("/tmp/luasys_install_" .. app.name)
+    dir = dir:gsub("^~", assert(os.getenv("HOME")))
+    local branch = app.branch or "master"
     local depth = "--depth=1"
     if app.full_depth then
         depth = "--single-branch"
     end
-    local branch = app.branch or "master"
-    if os.execute("test -d " .. dir) then
-        assert(os.execute(string.format("git -C %q pull", dir)))
-    else
-        assert(
-            os.execute(
-                string.format("git clone --branch %q %s %q %q", branch, depth, app.repo, dir)
+    if app.repo then
+        if os.execute("test -d " .. dir) then
+            assert(os.execute(string.format("git -C %q pull", dir)))
+        else
+            assert(
+                os.execute(
+                    string.format("git clone --branch %q %s %q %q", branch, depth, app.repo, dir)
+                )
             )
-        )
+        end
     end
     if app.diff then
         -- If we provide the filename as an arg, it will try to find the file relative to dir.
@@ -27,8 +38,19 @@ function App(app)
             return
         end
     end
+    if app.zig then
+        assert(os.execute(string.format("cd %q; zig build -Doptimize=ReleaseSafe", dir)))
+        assert(os.execute(string.format("cd %q; sudo mv -i zig-out/bin/* /usr/local/bin/", dir, app.name)))
+        return
+    end
+    if app.cmake then
+        assert(os.execute(string.format("mkdir %q/build; cd %q/build; cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local ..", dir, dir)))
+        assert(os.execute(string.format("cd %q/build; sudo make -j8 install", dir)))
+        return
+    end
 
-    local make_cmd = string.format("sudo make %s --directory %q", app.make_target or "install", dir)
+    local make_cmd =
+        string.format("sudo make -j8 %s --directory %q", app.make_target or "install", dir)
     if app.make_args then
         make_cmd = make_cmd .. " " .. app.make_args
     end
