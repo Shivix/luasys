b6a7ffc6b762f40b9c9c2126c6d1a16be41fdc36
Mark Oborne <markoborne1@gmail.com>
2026-03-18 22:02:45 +0900

    feat: Add codex and luarocks install

--- apps.lua
+++ apps.lua
@@ -71,6 +71,8 @@ App {
 App {
     name = "dwm",
     repo = "https://git.suckless.org/dwm",
+    -- TODO: diff won't apply cleanly past this version.
+    branch = "6.6",
     diff = "diffs/dwm.diff",
 }
 
@@ -95,7 +97,7 @@ App {
     name = "prefix",
     repo = "https://github.com/Shivix/prefix.git",
     cargo = true,
-    extra = "make install-fish",
+    extra = "make install-fish && make install-man",
 }
 
 App {
@@ -103,4 +105,34 @@ App {
     repo = "https://github.com/Shivix/fp.git",
     zig = true,
     extra = "make install-completion",
+}
+
+App {
+    name = "luarocks",
+    custom = function()
+        assert(os.execute("curl -O https://luarocks.org/releases/luarocks-3.13.0.tar.gz"))
+        assert(os.execute("tar zxpf luarocks-3.13.0.tar.gz"))
+        assert(os.execute("cd luarocks-3.13.0 && ./configure && make && sudo make install"))
+        assert(os.execute("rm luarocks-3.13.0.tar.gz"))
+        assert(os.execute("rm -rf luarocks-3.13.0"))
+    end,
+}
+
+App {
+    name = "codex",
+    custom = function()
+        local archive = "codex.tar.gz"
+        local bin_name = "codex-x86_64-unknown-linux-musl"
+
+        local url =
+            "https://github.com/openai/codex/releases/latest/download/codex-x86_64-unknown-linux-musl.tar.gz"
+        assert(os.execute(string.format("curl -fL %q -o %q", url, archive)))
+
+        assert(os.execute(string.format("tar -xzf %q %q", archive, bin_name)))
+
+        assert(os.execute(string.format("sudo mv %q /usr/local/bin/codex", bin_name)))
+        assert(os.execute("sudo chmod +x /usr/local/bin/codex"))
+
+        assert(os.execute(string.format("rm %q", archive)))
+    end,
 }
